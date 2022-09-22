c66cb3d5f341040f9a5eb126b0899bffbdb071b4
Gabriel S. Almeida <gabriel@ans.com.br>
2022-09-22 11:07:14 -0300

    fix: wrong import

--- pyqurl/web/flask.py
+++ pyqurl/web/flask.py
@@ -1,13 +1,13 @@
 import functools
 from flask import request
 
-from pyqurl.core import create_query_from_args
+from pyqurl.core import create_query_from_dict
 
 def use_query_filters():
     def wrapper(fn):
         @functools.wraps(fn)
-        def wrapped(*args, **kwargs):        
-            query = create_query_from_args(request.args)    
+        def wrapped(*args, **kwargs):
+            query = create_query_from_dict(request.args)
             return fn(*args, **kwargs, query=query)
         return wrapped
     return wrapper
