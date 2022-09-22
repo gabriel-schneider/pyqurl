1429df824b879eb66137db759684d9d33a1ef7d7
Gabriel S. Almeida <gabriel@ans.com.br>
2022-09-22 11:27:24 -0300

    fix: async/await for quart decorator

--- pyqurl/web/quart.py
+++ pyqurl/web/quart.py
@@ -6,8 +6,8 @@ from pyqurl.core import create_query_from_dict
 def use_query_filters():
     def wrapper(fn):
         @functools.wraps(fn)
-        def wrapped(*args, **kwargs):
+        async def wrapped(*args, **kwargs):
             query = create_query_from_dict(request.args)
-            return fn(*args, **kwargs, query=query)
+            return await fn(*args, **kwargs, query=query)
         return wrapped
     return wrapper
